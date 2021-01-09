182eca67ee591f6243885e83e04608cbfbe6bbfc
Keshox <keshoxvol@gmail.com>
2021-01-09 16:02:21 +0300

    first commit

---
+++ src/main/java/ru/codovstvo/srvadmin/SrvadminApplication.java
@@ -0,0 +1,13 @@
+package ru.codovstvo.srvadmin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SrvadminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SrvadminApplication.class, args);
+	}
+
+}
